dcb016544260e4e4deebcdaaca55f81bd6f37a23
Your Name <alexandra.mukhamedova@gmail.com>
2025-03-27 21:14:13 +0000

    Revert "404 error"
    This reverts commit 353f7b939d4896194d52b21f35b7720aad9af980.

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,33 +1,16 @@
 import Btn from "../../components/Btn/Btn";
 
-import { Helmet } from "react-helmet-async";
-
-import css from "./NotFoundPage.module.css";
-
-import { BASE_TITLE, NOT_FOUND_PAGE_TITLE } from "@constants/pages";
-
-const NotFoundPage = () => {
+export default function NotFound() {
   return (
-    <>
-      <Helmet>
-        <title>
-          {BASE_TITLE} - {NOT_FOUND_PAGE_TITLE}
-        </title>
-      </Helmet>
-      <div className={css.container}>
-        <h1 className={css.title}>404</h1>
-        <p className={css.message}>Oops! Page not found</p>
-        <p className={css.description}>
-          The page you&apos;re looking for doesn&apos;t exist or has been moved.
-        </p>
-        <Btn
-          tag="Go Home"
-          variant="back"
-          onClick={() => (window.location.href = "/")}
-        />
-      </div>
-    </>
-  );
-};
+    <div>
+      <h3>404 - Page Not Found</h3>
+      <p>We are sorry, but the page you are looking for does not exist.</p>
 
-export default NotFoundPage;
+      <Btn
+        tag="Go Home"
+        variant="back"
+        onClick={() => (window.location.href = "/")}
+      />
+    </div>
+  );
+}
