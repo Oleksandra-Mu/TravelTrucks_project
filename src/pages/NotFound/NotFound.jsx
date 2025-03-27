38580646524b7644f5078a43c0098dcc4c0ff826
Your Name <alexandra.mukhamedova@gmail.com>
2025-03-27 21:49:00 +0000

    404 error

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,14 +1,15 @@
+import css from "./NotFound.module.scss";
 import Btn from "../../components/Btn/Btn";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className={css.wrapper}>
       <h3>404 - Page Not Found</h3>
       <p>We are sorry, but the page you are looking for does not exist.</p>
 
       <Btn
         tag="Go Home"
-        variant="back"
+        variant="load"
         onClick={() => (window.location.href = "/")}
       />
     </div>
