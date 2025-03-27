353f7b939d4896194d52b21f35b7720aad9af980
Your Name <alexandra.mukhamedova@gmail.com>
2025-03-27 20:46:21 +0000

    404 error

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,16 +1,33 @@
 import Btn from "../../components/Btn/Btn";
 
-export default function NotFound() {
-  return (
-    <div>
-      <h3>404 - Page Not Found</h3>
-      <p>We are sorry, but the page you are looking for does not exist.</p>
+import { Helmet } from "react-helmet-async";
 
-      <Btn
-        tag="Go Home"
-        variant="back"
-        onClick={() => (window.location.href = "/")}
-      />
-    </div>
+import css from "./NotFoundPage.module.css";
+
+import { BASE_TITLE, NOT_FOUND_PAGE_TITLE } from "@constants/pages";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>
+          {BASE_TITLE} - {NOT_FOUND_PAGE_TITLE}
+        </title>
+      </Helmet>
+      <div className={css.container}>
+        <h1 className={css.title}>404</h1>
+        <p className={css.message}>Oops! Page not found</p>
+        <p className={css.description}>
+          The page you&apos;re looking for doesn&apos;t exist or has been moved.
+        </p>
+        <Btn
+          tag="Go Home"
+          variant="back"
+          onClick={() => (window.location.href = "/")}
+        />
+      </div>
+    </>
   );
-}
+};
+
+export default NotFoundPage;
