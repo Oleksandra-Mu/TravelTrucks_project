0bd233e741c7dd1a798e4d27792fb2d9d96991e6
Your Name <alexandra.mukhamedova@gmail.com>
2025-04-02 19:55:57 +0100

    Removed extra tags

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,34 +1,31 @@
 import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.scss";
 import clsx from "clsx";
+import Icons from "../Icons/Icons";
 
 const Navigation = () => {
   const getActiveClass = ({ isActive }) => {
     return clsx(css.navLink, isActive && css.active);
   };
   return (
-    <header>
-      <div className={css.wrapper}>
-        <NavLink to="/" className={css.logo}>
-          <svg width="132" height="16" viewBox="0 0 132 16">
-            <use href="/icons.svg#icon-logo"></use>
-          </svg>
-        </NavLink>
-        <nav className={css.navigation}>
-          <ul>
-            <li>
-              <NavLink className={getActiveClass} to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className={getActiveClass} to="/catalog">
-                Catalog
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header className={css.wrapper}>
+      <NavLink to="/" className={css.logo}>
+        <Icons name={"logo"} width={132} height={16} />
+      </NavLink>
+      <nav className={css.navigation}>
+        <ul>
+          <li>
+            <NavLink className={getActiveClass} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={getActiveClass} to="/catalog">
+              Catalog
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
